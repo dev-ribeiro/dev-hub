12a8ba2d31772a232a103d11cd61ce66ba95bbb1
David Ribeiro da Silva <david.ribeiro.professional@gmail.com>
2024-03-20 13:36:27 -0300

    feat: implement feed in web

--- web/src/layout/AppLayout.tsx
+++ web/src/layout/AppLayout.tsx
@@ -5,16 +5,12 @@ export function AppLayout() {
   useIsAuthenticatedUser()
 
   return (
-    <div className="min-h-screen flex flex-col bg-sky-100">
-      <header className="py-5 px-2 bg-sky-600">
+    <div className="min-h-screen flex flex-col items-center bg-sky-100">
+      <header className="self-stretch py-5 px-2 bg-sky-600">
         <h2 className="text-2xl font-bold text-white">DEV HUB</h2>
       </header>
-      <div className="px-2 py-3 flex-1 flex gap-5">
-        <aside className="flex-[1]"></aside>
-        <div className="flex-[3] flex flex-col border-[1px] border-solid border-sky-800 rounded">
-          <Outlet />
-        </div>
-        <aside className="flex-[1]"></aside>
+      <div className="m-3 flex flex-col items-start border-[1px] border-solid border-sky-800 rounded">
+        <Outlet />
       </div>
     </div>
   );
--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -19,29 +19,40 @@ export function HomePage() {
   const { handleSubmit, register } = postForm
 
   const emptyList = (
-    <h2 className="text-2xl font-bold">NÃO HÁ POSTS</h2>
+    <div className="flex-1 flex justify-center items-center">
+      <h2 className="text-2xl font-bold">NÃO HÁ POSTS</h2>
+    </div>
   );
 
   const gridPost = (
-    <section className="w-full flex flex-col">
-      <div className="w-full py-5">
+    <section className="flex-1 flex flex-col">
+      <div className="pb-5">
         <h2 className="font-bold text-xl">Seu feed</h2>
       </div>
-      <div className="max-w-full space-y-2 bg-red-50">
-        {posts.map(post => (
-          <div key={post.id} className="w-full flex gap-2 border-b-[1px] border-solid border-sky-500">
-            <div>
-              <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
+      <div className="space-y-5">
+        {posts.map(post => {
+          const createdAtFormated = new Date(post.createdAt)
+            .toLocaleDateString('pt-BR', { hour: '2-digit', minute: '2-digit' })
+
+          return (
+            <div key={post.id}
+              className="flex-1 pb-2 flex gap-2 border-b-[1px] border-solid border-sky-500">
+              <div className="w-10 h-10">
+                <Avatar>
+                  <AvatarImage src="https://github.com/shadcn.png" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </div>
+              <div className="w-[90%]">
+                <h3 className="font-bold text-justify">{post.title}</h3>
+                <span className="max-w-full inline-block break-words">{post.description}</span>
+                <p className="text-end">
+                  Postado por <strong>{post.user.firstName}</strong>, em {createdAtFormated}
+                </p>
+              </div>
             </div>
-            <div className="flex flex-col text-wrap">
-              <h3 className="font-bold text-justify">{post.title}</h3>
-              <div>{post.description}</div>
-            </div>
-          </div>
-        ))}
+          )
+        })}
       </div>
     </section>
   );
@@ -50,7 +61,7 @@ export function HomePage() {
     <div className="absolute bottom-[3vh] left-[3vw]">
       <Dialog open={createModalVisibility} onOpenChange={() => setCreateModalVisibility(!createModalVisibility)}>
         <DialogTrigger onClick={() => setCreateModalVisibility(true)}>
-          <button className="p-3 bg-sky-600 rounded">
+          <button className="p-3 fixed bottom-[2vh] left-[1vw] bg-sky-600 rounded">
             <span className="text-white font-bold">CRIAR POST</span>
           </button>
         </DialogTrigger>
@@ -79,12 +90,10 @@ export function HomePage() {
   );
 
   return (
-    <>
-      <main className="p-2 flex-1 flex">
-        {posts.length === 0 && emptyList}
-        {posts.length > 0 && gridPost}
-      </main>
+    <main className="p-3 max-w-[300px] md:max-w-screen-md">
+      {posts.length === 0 && emptyList}
+      {posts.length > 0 && gridPost}
       {dialogPost}
-    </>
+    </main>
   );
 }
